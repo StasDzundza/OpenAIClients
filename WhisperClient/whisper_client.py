683ea812893f359777cc2f1183aa1dae9f04ecc8
Stanislav Dzundza <stas.dzundza@gmail.com>
2023-04-24 23:44:18 +0300

    significantly improve whisper client, use ffmpeg

--- WhisperClient/whisper_client.py
+++ WhisperClient/whisper_client.py
@@ -1,7 +1,7 @@
-import openai
 import logging
-from dataclasses import dataclass
-from enum import Enum
+import openai
+import os
+import subprocess
 
 # response example
 # {
@@ -14,17 +14,42 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+def get_file_extension(filename: str) -> str:
+    return filename.split(".")
+
 class WhisperClient:
     @staticmethod
-    def transcript_media_file(api_key:str, path_to_file: str) -> str:
+    def transcript_media_file(api_key: str, path_to_file: str) -> str:
+        filename = WhisperClient.convert_to_supported_format(path_to_file)
+        converted = filename != path_to_file
+        media_file = open(filename, "rb")
         try:
-            media_file = open(path_to_file, "rb")
             transcript_response = openai.Audio.transcribe(
                 api_key = api_key,
                 model = "whisper-1",
                 file = media_file
             )
-            return transcript_response.data["text"]
+            media_file.close()
+            if converted: os.remove(filename)
+            return transcript_response["text"]
         except Exception as e:
+            media_file.close()
+            if converted: os.remove(filename)
             logger.error(f"Error generating Whisper response: {e}")
-            return "An error occurred while generating a response. Please try again."
+            return str(e)
+
+
+    @staticmethod
+    def get_supported_file_formats() -> list[str]:
+        return ['m4a', 'mp3', 'webm', 'mp4', 'mpga', 'wav', 'mpeg']
+
+    @staticmethod
+    def convert_to_supported_format(path_to_file: str) -> str:
+        file_extension = get_file_extension(path_to_file)
+        if file_extension[1] not in WhisperClient.get_supported_file_formats():
+            output_file = f"{file_extension[0]}.wav"
+            command = f"ffmpeg -y -i {path_to_file} -ac 1 -ar 16000 {output_file}"
+            subprocess.call(command, shell=True)
+            return output_file
+        else:
+            return path_to_file
--- whisper_example.py
+++ whisper_example.py
@@ -6,5 +6,5 @@ def run_example_request(api_key: str, media_file_path: str):
 
 if __name__ == "__main__":
     api_key = "YOUR_API_KEY" # use your API key here
-    path_to_media_file = "MEDIA_FILE_PATH" # use path to some media file here
+    path_to_media_file = "path_to_media_file.mp3" # use path to some media file here
     run_example_request(api_key, path_to_media_file)
